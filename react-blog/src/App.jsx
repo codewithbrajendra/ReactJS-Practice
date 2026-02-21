d788aefaa995f70e8634abb6bf5191330540dbed
codewithbrajendra <brajendra0523@gmail.com>
2026-02-21 16:25:32 +0530

    First react blog project with simple component in App.jsx

---
+++ react-blog/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from "./Header"
+import React from "react";
+import './App.css'
+
+export const App = () => {
+  return (
+        <div>
+        <img src="/images/qot.jpg" alt="qot" className='movie-img'></img>
+        <h2>Name : Queen of Tears</h2>
+        <h3>Rating:8.2</h3>
+        <p>
+        Summary: Baek Hyun-woo and Hong Hae-in navigate a tense.......
+        </p>
+        </div>
+  );
+};
+
+const NetflixSeries = () => {
+
+}
+
