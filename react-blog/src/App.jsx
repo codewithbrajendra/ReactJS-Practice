d6f16dbca3f90532221ee0d324913cd4ebe34a48
codewithbrajendra <brajendra0523@gmail.com>
2026-03-01 15:41:25 +0530

    Looping in JSX for displaying multiple Netflix cards in single page

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -1,19 +1,13 @@
 
 import './App.css'
 
-import NetflixSeries, {Header, Footer} from "./components/NetflixSeries"
+import NetflixSeries from "./components/NetflixSeries"
 
 export const App = () => {
 
 return (
     <>
-      <Header/>
       <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <Footer/>
    </>
   );
 
--- react-blog/src/components/NetflixSeries.jsx
+++ react-blog/src/components/NetflixSeries.jsx
@@ -1,37 +1,28 @@
+import seriesData from "../api/seriesData.json"
+
 const NetflixSeries = () => {
-  const name = "Queen of Tears";
-  const summary = "A gripping tale of love, loss, and resilience set against the backdrop of a war-torn nation.";
-  let age = 19;
-  // 3. let canWatch = "Not Available";
-  // 3. if(age > 18) canWatch = "Watch Now";
-  const returnGenre = () => {
-    const genre = "RomCom";
-    return genre;
-  }
-  const canWatch = () => {
-    if (age > 18) return "Watch Now";
-    return "Not Available"
-  }
+
 return (
-        <div>
-        <img src="/images/qot.jpg" alt="qot" className='movie-img'></img>
-        <h2>Name : {name} </h2>
-        <h3>Rating:{12/2+2.2}</h3>
-        <p>{summary}</p>
-        <p>Genre : {returnGenre()} </p>
-        {/* 2. <button className="btn"> {age > 18 ? "Watch Now" : "Not Available"} </button> */}
-        {/* 3. <button className="btn"> {canWatch} </button> */}
-        <button className="btn" > {canWatch()} </button>
-        </div>
+    <ul>
+      {seriesData.map((currElem) => {
+        return (
+        <li key={currElem.id}>
+            <div>
+                <img src={currElem.img_url} alt={currElem.name} className='movie-img'></img>
+            </div>
+            <h2>Name : {currElem.name} </h2>
+            <h3>Rating:{currElem.rating}</h3>
+            <p>Summary: {currElem.description}</p>
+            <p>Genre : {currElem.genre} </p>
+            <a href={currElem.watch_url} target="_blank">
+                <button className="btn">Watch Now</button>
+            </a>
+        </li>
+        )
+        }
+      )}
+    </ul>
   );
 }
 
 export default NetflixSeries;
-
-export const Header = () => {
-    return <p>This is Header Component of NetflixSeries.</p>
-}
-
-export const Footer = () => {
-    return <p>copyright brjanedra0523@gmail.com</p>
-}
