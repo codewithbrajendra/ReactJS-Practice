e8e598204c4692d7595dd43160e917d6c3130efc
codewithbrajendra <brajendra0523@gmail.com>
2026-03-04 18:10:18 +0530

    Adding css and inernal css for good lokin card

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -1,13 +1,21 @@
 
-import './App.css'
+// import './App.css'
+import './index.css'
+import './components/Netflix.css'
 
 import NetflixSeries from "./components/NetflixSeries"
 import { ProfileCard } from './components/ProfileCard';
 
 export const App = () => {
 
-return <ProfileCard />
-{/* <NetflixSeries />; */}
+return (
+    <section className='container'>
+        <h1 className='card-heading'>List of Best Netflix Series</h1>
+      <NetflixSeries />
+    </section>
+);
+    
+
 
 
 };
